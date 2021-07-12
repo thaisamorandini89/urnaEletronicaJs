91baa8145c1356f347ab8313cb71bbf8ab73a04c
unknown <thaisamorandini@gmail.com>
2021-07-12 19:22:00 -0300

    Array de candidatos

---
+++ etapas.js
@@ -0,0 +1,58 @@
+let etapas = [
+
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '38111',
+                nome: 'Lucas de tal',
+                partido: 'ABC',
+                fotos:[
+                    {url:'38111.jpg', legenda: 'Vereador'}
+                ]
+            },
+
+            {
+                numero: '77222',
+                nome: 'Beltrano da silva',
+                partido: 'DEFG',
+                fotos:[
+                    {url: '77222.jpg', legenda: 'Vereador'}
+                ]
+            },
+        ]
+    },
+
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos:[
+            {
+                numero: '99',
+                nome: 'Ciclano',
+                partido: 'ABC',
+                vice: 'Cic',
+                fotos:[
+                    {url: '99.jpg', legenda: 'Prefeito'},
+                    {url: '99_2.jpg', legenda: 'Vice-prefeito', small: true}
+                ]
+            },
+
+            {
+                numero: '84',
+                nome: 'Zulano',
+                partido: 'QWERTY',
+                vice: 'Zul',
+                fotos:[
+                    {url:'84.jpg', legenda: 'Prefeita'},
+                    {url:'84_2.jpg', legenda: 'vice-Prefeito', small: true}
+                ]
+            },
+
+        ]
+
+    }
+
+];
+
